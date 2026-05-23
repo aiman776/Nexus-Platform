13e603c26d926b4f29bf6567aed3f93851a4b44d
aiman776 <147138091+aiman776@users.noreply.github.com>
2026-05-23 16:54:54 +0500

    ready the Folder Structure

---
+++ PITCHNEST/backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
+
+const PORT = 1000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
